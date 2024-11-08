f0a3b61d3964e03c6a5980b4cfbc4569558e165b
Elion-Meha <elion.meha@laetottenham.ac.uk>
2024-11-08 11:59:35 +0000

    created basic cat class

---
+++ main.py
@@ -0,0 +1,10 @@
+class Cat:
+    def __init__(self, given_name, given_color):
+        #     The constructor - creates and instance of a class
+        #  -  To create the cat , we need a given_name and given_color
+        self.name = given_name
+        self.colour = given_colour
+# An instance of a Cat
+# An instance is a specific occurrence of a class
+mimi = Cat("Mimi","Brown")
+
